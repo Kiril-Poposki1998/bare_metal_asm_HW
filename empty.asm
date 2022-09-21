26a05a29cd72dd8542ad322a7b154b3e6930c21b
Kiril Poposki <kirilpoposki@hotmail.com>
2022-09-21 11:56:36 +0200

    PE hello world assembly

---
+++ empty.asm
@@ -0,0 +1,16 @@
+format pe64 efi
+entry main
+
+section '.text' executable readable
+
+main:
+  mov rcx, [rdx + 64]
+  mov rax, [rcx + 8]
+  mov rdx, string
+  sub rsp, 32
+  call rax
+  add rsp, 32
+  ret
+
+section '.data' readable
+string du 'Hello World!', 0xD, 0xA, 0x0
